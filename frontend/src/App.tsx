79934054c2cb993a546a22b3cf60220c08c70571
userwei <chou.ting.wei@outlook.com>
2024-04-18 21:41:06 +0800

    fix bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { CookiesProvider, useCookies } from 'react-cookie'
 
 import EditUser from './assets/EditUser'
