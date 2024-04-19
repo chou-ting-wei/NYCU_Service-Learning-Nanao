00e88bbd33607c3004940118f90dc566ae7fce02
Hans <hansliao2015@gmail.com>
2024-04-19 13:03:36 +0800

    Test shadcn ui

--- frontend/src/assets/Interact/Interact.tsx
+++ frontend/src/assets/Interact/Interact.tsx
@@ -1,9 +1,11 @@
 import './Interact.css'
+import { Button } from "@/components/ui/button"
 
 const Interact = () => { 
     return (
       <div className="interact">
         <p>Interact Page Test</p>
+        <Button>Test Button</Button>
       </div>
     );
   }
