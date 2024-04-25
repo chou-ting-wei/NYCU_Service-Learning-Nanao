8f199a485026301a94d8ee78df956eb176f67b16
shangjung1012 <david103132881@gmail.com>
2024-04-25 22:48:50 +0800

    add profile

--- frontend/src/assets/Profile.tsx
+++ frontend/src/assets/Profile.tsx
@@ -1,9 +1,18 @@
 import './Profile.css'
+import { Link } from 'react-router-dom';
 
 const Profile = () => { 
   return (
     <div className="profile">
-        <p>Profile Page Test</p>
+      <img src="image.jpg" alt="Profile Picture" />
+        <div className="info">
+          <div><span className="label">姓名：</span>王小明</div>
+          <div><span className="label">性別：</span>男</div>
+          <div><span className="label">生日：</span>1990-01-01</div>
+          <div><span className="label">年齡：</span>33</div>
+          <div><span className="label">過去病史：</span>無</div>
+          <Link to="/edit-user">修改會員資料</Link>
+        </div>
     </div>
   );
 }
