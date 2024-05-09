1663deb0970a220819363463fe5211f0229100aa
shangjung1012 <david103132881@gmail.com>
2024-05-10 03:52:44 +0800

    edit home page

--- frontend/src/assets/Home.tsx
+++ frontend/src/assets/Home.tsx
@@ -3,7 +3,9 @@ import './Home.css'
 const Home = () => { 
   return (
     <div className="home">
-        <p>Home Page Test</p>
+        {/* <h1>Home Page</h1> */}
+        <br />
+        <img src="/public/home_photo.jpg" alt="Home Photo" />
     </div>
   );
 }
