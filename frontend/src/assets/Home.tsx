68684ebd689d2708c767204992f7a5bff2a9df1d
shangjung1012 <david103132881@gmail.com>
2024-05-21 22:54:38 +0800

    add home

--- frontend/src/assets/Home.tsx
+++ frontend/src/assets/Home.tsx
@@ -1,11 +1,13 @@
 import './Home.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => { 
   return (
     <div className="home">
-        {/* <h1>Home Page</h1> */}
-        {/* <br /> */}
-        {/* <img src="/home_photo.jpg" alt="Home Photo" /> */}
+      <Link to="/login">
+        <button className="login-button">登入</button>
+      </Link>
     </div>
   );
 }
