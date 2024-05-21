8134e959fcb39873345abd1f4a22495055d21f76
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-05-21 22:07:25 +0800

    fix login and logout

--- frontend/src/assets/Logout.tsx
+++ frontend/src/assets/Logout.tsx
@@ -1,17 +1,31 @@
-import './Home.css'
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Logout = ({ onLogin }) => {
+const Logout = ({ onLogin, url }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const logout = async () => {
       onLogin(null, null);
 
-      navigate('/home');
-  }, [onLogin, navigate]);
+      try {
+        await axios.delete(url + 'auth/logout', {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        navigate('/home');
+      } catch (error) {
+        console.error(error);
+        // Optionally update the UI to inform the user
+      }
+    };
 
-  return <p>登出中...</p>;
+    logout();
+  }, [onLogin, navigate, url]); // Include `url` if it can change during component life
+
+  return <p>登出中...</p>; // Displays while logging out
 }
 
 export default Logout;
