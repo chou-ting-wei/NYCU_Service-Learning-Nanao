b224bcf8d35380f18bfad64e131327c51e28c5b3
userwei <70355885+chou-ting-wei@users.noreply.github.com>
2024-05-25 01:28:53 +0800

    fix pushImage

--- pushImage.sh
+++ pushImage.sh
@@ -1,7 +1,6 @@
 docker-compose build
 
-docker tag myproject_backend userwei/nycu_service-learning-nanao:backend
-docker tag myproject_frontend userwei/nycu_service-learning-nanao:frontend
-
 docker push userwei/nycu_service-learning-nanao:backend
 docker push userwei/nycu_service-learning-nanao:frontend
+
+docker-compose up -d
